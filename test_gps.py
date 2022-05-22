96661af6e467067bb5e05d79750ec47f38c90ffb
Brian Nieto <brian.nieto1@utp.ac.pa>
2022-05-22 00:21:54 -0500

    [GPS] Testing gps function

--- test_gps.py
+++ test_gps.py
@@ -3,7 +3,7 @@ import time
 
 ser = serial.Serial("/dev/ttyUSB0", 115200)
 W_buff = ["AT+CGNSPWR=1\r\n", "AT+CGNSSEQ=\"RMC\"\r\n", "AT+CGNSINF\r\n", "AT+CGNSURC=2\r\n", "AT+CGNSTST=1\r\n"]
-ser.write(W_buff[0])
+ser.write(W_buff[0].encode())
 ser.flushInput()
 data=""
 num=0
@@ -14,11 +14,11 @@ try:
         if(data != ""):
             print(data)
             time.sleep(0.5)
-            ser.write(W_buff[num+1])
+            ser.write(W_buff[num+1].encode())
             num = num + 1
             if(num == 4):
                 time.sleep(0.5)
-                ser.write(W_buff[4])
+                ser.write(W_buff[4].encode())
             data = ""
             
 except KeyboardInterrupt:
