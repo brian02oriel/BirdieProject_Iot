a0961531f48619e1b0b64ca3ce3623af1117f793
Brian Nieto <brian.nieto1@utp.ac.pa>
2022-05-22 22:11:29 -0500

    [GPS] Testing gps function

--- test_gps.py
+++ test_gps.py
@@ -10,12 +10,11 @@ num=0
 try:
     while True:
         while ser.inWaiting() > 0:
-            #to_string = ''.join(map(chr, byte))
             data += ser.read(ser.inWaiting()).decode()
         if(data != ""):
             print(data)
             time.sleep(0.5)
-            ser.write(W_buff[num+1].encode())
+            ser.write(W_buff[num].encode())
             num = num + 1
             if(num == 4):
                 time.sleep(0.5)
