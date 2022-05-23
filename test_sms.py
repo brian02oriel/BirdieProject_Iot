d643d47cbd7211a3a913bfcf3dd41c4825eb71b6
Brian Nieto <brian.nieto1@utp.ac.pa>
2022-05-22 23:27:21 -0500

    SMS example

--- test_sms.py
+++ test_sms.py
@@ -2,14 +2,27 @@ import serial
 import time
 
 ser = serial.Serial("/dev/ttyUSB0", 115200)
-ser.write("AT\r\n".encode())
+
+W_buff = ["AT\r\n", "AT+CMGF=1\r\n", "AT+CSCA=\"+8613800755500\"\r\n", "AT+CMGS=\"+50765883374\"\r\n", "HELLO WORLD"]
+ser.write(W_buff[0].encode())
 ser.flushInput()
-ser.write("AT+CMGF=1\r\n".encode())
-ser.flushInput()
-ser.write("AT+CSCA=\"+8613800755500\"\r\n".encode())
-ser.flushInput()
-ser.write("AT+CMGS=\"+50765883374\"\r\n".encode())
-ser.flushInput()
-ser.write("HELLO WORLD".encode())
-ser.flushInput()
-time.sleep(0.5)
+data=""
+num=0
+try:
+    while True:
+        while ser.inWaiting() > 0:
+            data += ser.read(ser.inWaiting()).decode()
+        if(data != ""):
+            print(data)
+            time.sleep(0.5)
+            ser.write(W_buff[num+1].encode())
+            num = num + 1
+            if(num == 4):
+                time.sleep(0.5)
+                ser.write(W_buff[4].encode())
+                num = 0
+            data = ""
+            
+except KeyboardInterrupt:
+    if(ser != None):
+        ser.close()
