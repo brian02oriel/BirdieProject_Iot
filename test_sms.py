b373525188bf3785378ddc461dbef0cb48edef49
Brian Nieto <brian.nieto1@utp.ac.pa>
2022-05-22 23:19:02 -0500

    SMS example

--- test_sms.py
+++ test_sms.py
@@ -3,8 +3,13 @@ import time
 
 ser = serial.Serial("/dev/ttyUSB0", 115200)
 ser.write("AT\r\n".encode())
+ser.flushInput()
 ser.write("AT+CMGF=1\r\n".encode())
+ser.flushInput()
 ser.write("AT+CSCA=\"+8613800755500\"\r\n".encode())
+ser.flushInput()
 ser.write("AT+CMGS=\"+50765883374\"\r\n".encode())
+ser.flushInput()
 ser.write("HELLO WORLD".encode())
 ser.flushInput()
+time.sleep(0.5)
